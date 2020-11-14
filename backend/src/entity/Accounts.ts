d8ed7e427bd630cfbae54326dec5355007306646
CursedGamerZ <Deadlydollar@hotmail.com>
2020-11-14 11:54:59 +0400

    mass addition
    *moved backend to backend folder
    *added frontend and added required libraries
    *added Posts Delete route
    * added API
    *started on context

---
+++ backend/src/entity/Accounts.ts
@@ -0,0 +1,25 @@
+import {Entity, Column,PrimaryColumn, CreateDateColumn} from "typeorm";
+
+@Entity()
+export class Account{
+    @PrimaryColumn()
+    name!: string;
+
+    @Column()
+    Hpass!: string;
+
+    @Column()
+    email!: string;
+
+    @Column()
+    bio!: string;
+
+    @Column()
+    birthday!: string;
+
+    @Column()
+    Role!: string;
+
+    @CreateDateColumn()
+    accountDOB!: string;
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,19 +8,15 @@ import { parse } from "path";
 import { JsonObjectExpression, parseJsonText } from "typescript";
 
 const app = express();
-
 app.use(cors());
-
 app.use(express.json());
-
 const port = 3001;
-
-
-// 
 
 createConnection().catch((err) => {
     throw new Error(err);
 });
+
+
 
 app.get("/posts", async (req, res) => {
     const posts = await getConnection().getRepository(Posts).find();
@@ -29,45 +25,80 @@ app.get("/posts", async (req, res) => {
     }
     return res.sendStatus(200).send(posts);
 });
-//
-app.post("/Posts", async (req, res) => {
+
+
+
+/*
+*   posting route
+*/
+app.post("/posts", async (req, res) => {
     const post = req.body as Posts;
 
-    if (!post.catagory|| !post.title|| !post.content) {
+    if (!post.catagory|| !post.title|| !post.content ) {
         return res.sendStatus(400);
     }
-
     if (post.title.length > 128 || post.content.length > 10000) {
         return res.sendStatus(413);
     }
-
-
     const sendIt = await getConnection().getRepository(Posts).save(post);
-
     if (!sendIt) {
         return res.sendStatus(500)
     }
     return res.status(200).send(post)
 });
 
-app.get("/search/:content", async (req, res) => {
-    const content = req.params;
-    const contentb = content.content
-    console.log(content);
+
 
 /*
-How do i tell program to stop adding shit forehead ?
+*   Delete Post Route
 */
+app.delete("/posts/:id", async (req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        return res.sendStatus(400);
+    }
+    const parseID = parseInt(id, 10);
+    const posts = await getConnection().getRepository(Posts).findOne({id: parseID});
+    if (!posts) {
+        return res.sendStatus(404);
+    }
+    const ok = await getConnection().getRepository(Posts).delete(posts);
+    if (!ok) {
+        return res.sendStatus(500);
+    }
+    return res.sendStatus(200);
+})
 
+
+
+
+
+/*
+*   Searching Route
+*/
+app.get("/search/:content", async (req, res) => {
+    const content = req.params;
     const firstUser = await getConnection()
     .getRepository(Posts)
     .createQueryBuilder("posts")
     .where("title Like :content", {"content"  : "%" + content.content + "%" } )
     .getMany();
-    return res.status(200).send (firstUser);
-
-
+    return res.status(200).send(firstUser);
 });
+
+
+
+/*
+*   New posts route
+*/
+app.get("/newests", async (req, res) => {
+        const newPosts = await getConnection()
+            .getRepository(Posts)
+            .createQueryBuilder("posts")
+            .orderBy("posts.date")
+            .getMany();
+        return res.status(200).send(newPosts);
+    })
 
 
 
